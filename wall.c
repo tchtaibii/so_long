79b3fd6771c21a7fc8cd92bbcade73ce3af47998
Taha Chtaibi <tchtaibi@e2r3p7.1337.ma>
2021-12-23 20:30:46 +0100

    brouillon0

--- wall.c
+++ wall.c
@@ -1,9 +1,20 @@
 #include "so_long.h"
-
+int ft_checkE(char *line)
+{
+	int c = 0;
+	while (line[j])
+	{
+		if(line[j] == 'E')
+			c++;
+		j++;
+	}
+	return c;
+}
 int ft_wall(int fd)
 {
 	char	*line;
 	int i = 0;
+	int j = 1;
 	line = get_next_line(fd);
 	while (line[i] != '\n')
 	{
@@ -15,5 +26,24 @@ int ft_wall(int fd)
 		i++;
 	}
 	free(line);
+	line = get_next_line(fd);
+	if (line[0] != '1')
+	{
+		write(1, "fix ur wall", 11);
+		return (0);
+	}
+	int e = ft_checkE(line);
+	if (e > 1 || e == 0 )
+	{
+		write(1, "fix ur exit", 11);
+		return 0;
+	}
+	while (line[j] != '\n')
+	{
+		
+		if (line[j] != '0' || line[j] != '')
+		i++;
+	}
+	
 	return i;	
 }
