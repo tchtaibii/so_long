3857d87a7e04be744ac37800c9631d262ba78376
Taha Chtaibi <tchtaibi@e2r4p9.1337.ma>
2021-12-24 14:04:06 +0100

    check map

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: tchtaibi <tchtaibi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/21 15:46:16 by tchtaibi          #+#    #+#             */
-/*   Updated: 2021/12/24 02:10:34 by tchtaibi         ###   ########.fr       */
+/*   Updated: 2021/12/24 14:03:29 by tchtaibi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,46 +62,74 @@ int main(int ac, char **av)
     int c = 0;
     int e = 0;
     int p = 0;
-    while (map[i])
+    int o = 0;
+    
+    if (ac == 2)
     {
-        if(map[i] == 'E')
-            e++;
-        if(map[i] == 'P')
-            p++;
-        if(map[i] == 'C')
-            c++;
-        if((map[i] == '\n' && map[i - 1] != '1') || (map[i] == '\n' && map[i + 1] != '1'))
+        while (map[i])
         {
-            write(1, "FIX UR EDGES", 12);
+            if(map[i] != '\n' && map[i] == '0' && map[i] == 'E' && map[i] == 'C' && map[i] == '1'&& map[i] == 'P')
+            {
+                write(1, "ERROR", 5);
+            }
+            if(map[i] == 'E')
+                e++;
+            if(map[i] == '0')
+                o++;
+            if(map[i] == 'P')
+                p++;
+            if(map[i] == 'C')
+                c++; 
+            if((map[i] == '\n' && map[i - 1] != '1') || (map[i] == '\n' && map[i + 1] != '1'))
+            {
+                write(1, "FIX UR EDGES", 12);
+                return 0;
+            }
+            i++;
+        }
+        if (c < 1 || e != 1 || p != 1 || o <= 0)
+        {
+            write(1, "ERROR", 5);
             return 0;
         }
-        i++;
-    }
-    i = 0;
-    if (c < 1 || e != 1 || p != 1)
-    {
-        write(1, "ERROR", 5);
+        i = 0;
+        while (map[i] != '\n')
+        {
+            if(map[i] != '1')
+            {
+                write(1, "fix ur up wall", 14);
+                return 0;
+            }
+            i++;
+        }
+        int j = i;
+        i *= 2;
+        while (map[j])
+        {
+            if (map[j] == '\n')
+            {
+                if (i != j)
+                {
+                    write(1, "fix ur map", 10);
+                    return 0;
+                }
+                else
+                    i *= 2;
+            }
+        }
+        i = ft_strlen1(map) - 1;
+        while (map[i] != '\n' && map[i])
+        {
+            if(map[i] != '1')
+            {
+                write(1, "fix ur low wall", 15);
+                return 0;
+            }
+            i--;
+        }
+        write(1, "ur map is great", 15);
         return 0;
     }
-    while (map[i] != '\n')
-    {
-        if(map[i] != '1')
-        {
-            write(1, "fix ur up wall", 14);
-            return 0;
-        }
-        i++;
-    }
-    i = ft_strlen1(map) - 1;
-    while (map[i] != '\n')
-    {
-        if(map[i] != '1')
-        {
-            write(1, "fix ur low wall", 15);
-            return 0;
-        }
-        i--;
-    }
-    write(1, "ur map is great", 15);
-    return 0;
+    else
+        write(1, "ERROR", 5);
 }
