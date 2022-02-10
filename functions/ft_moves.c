9614da6033c6220b718b3c22cb2f0122d35782ac
Taha Chtaibi <tchtaibi@e2r5p10.1337.ma>
2022-02-10 15:05:33 +0100

    kayn a ali 2

--- functions/ft_moves.c
+++ functions/ft_moves.c
@@ -3,10 +3,16 @@ void ft_move_down(int key, t_img *img)
 {
 	if (key == 1 || key == 125)
 	{
-		if(global_map[img->b + 1][img->a] != '1' || (global_map[img->b + 1][img->a] == 'E' && img->cn == img->cnf))
+		if(global_map[img->b + 1][img->a] != '1' && global_map[img->b + 1][img->a] != 'E')
 		{
 			if(global_map[img->b + 1][img->a] == 'C')
 				img->cn++;
+			mlx_put_image_to_window(img->ptr_mlx, img->window_mlx, img->empty, img->a * 68, img->b * 68);
+			img->b++;
+			mlx_put_image_to_window(img->ptr_mlx, img->window_mlx, img->player, img->a * 68, img->b * 68);
+		}
+		else if (global_map[img->b + 1][img->a] == 'E' && img->cn == img->cnf)
+		{
 			mlx_put_image_to_window(img->ptr_mlx, img->window_mlx, img->empty, img->a * 68, img->b * 68);
 			img->b++;
 			mlx_put_image_to_window(img->ptr_mlx, img->window_mlx, img->player, img->a * 68, img->b * 68);
@@ -37,6 +43,11 @@ int	ft_move_player(int key, t_img *img)
 {
 	ft_move_down(key, img);
 	ft_move_up(key, img);
+	if (key == 53)
+	{
+		mlx_destroy_window(img->ptr_mlx, img->window_mlx);
+		exit(1);
+	}
 
 	return 0;
 }
